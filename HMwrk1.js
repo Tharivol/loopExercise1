f6e9491eaad218f8df643bfc9ac543e8c8bf4161
Robert Hughes <hughesbkr@yahoo.com>
2016-11-30 16:37:47 -0700

    initial commit

---
+++ HMwrk1.js
@@ -0,0 +1,44 @@
+//Homework 1LOOP
+
+//for(var i = 0; i<=500; i+=100)	{
+//	console.log (i);
+//}
+
+	
+//for(var i = 1; i<=32; i+=i)	{	
+//	console.log (i);
+//}
+	
+//for( var i = 1; i<=3; i++)	{
+	//for( var j = 1; j<=3; j++) {
+	//console.log (i);
+	//}
+//}
+
+//for( var i = 0; i<=10; i+=2)	{
+	//console.log (i);
+//}
+
+//for(var i = 3; i<=15; i+=3)	{
+	//console.log (i);
+//}
+//}
+
+//for(var i =10; i>=0; i--)	{
+	//console.log(i)
+//}
+
+for(var i=0; i<=3; i++)	{
+	for(var j=0; j<3; j++) {
+		console.log(j);
+	}}
+
+
+
+
+
+
+
+
+	process.exit(0)
+
---
+++ JSprobset1.js
@@ -0,0 +1,9 @@
+//JSprobset1
+
+
+function letterCapitalize()	{
+var str= ucwords("hello i am rob");
+		console.log(str);
+		}
+
+
