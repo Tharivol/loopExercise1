2a4d09712c2493f8ad6ff46bbbeb2178d17da40f
Robert Hughes <hughesbkr@yahoo.com>
2016-11-30 17:35:20 -0700

    looping

---
+++ forloops2.js
@@ -0,0 +1,24 @@
+//forloops2.js
+
+var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+
+//for(var i=0; i<animals.length - 1; i++) 	{
+	//console.log(animals[i]);
+//}
+
+//for (var i = 0; i < animals.length; i++) {
+	//if(i % 2 === 0) {
+	//	console.log(animals[i]);
+	//}
+//}
+
+//for (var i = animals.length-1; i >=0;  i--) {
+	//console.log(animals[i]);
+//}
+
+//for(var i=0; i<animals.length; i++) {
+	//if(i != 0 && i != animals.length-1){
+		//console.log(animals[i]);
+	//} 
+	//console.log(animals[i]);
+//}
